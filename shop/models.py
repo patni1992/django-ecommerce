4d0fd3783e3815074724caa7efbaa8e73ae74b38
Patrik <patriknilsson1992@gmail.com>
2019-01-17 19:46:12 +0700

    add Product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True, db_index = True)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
