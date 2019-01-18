aea066ea0742de8f45fbfd1b3dcaa6fa5640b611
Patrik <pn222en@student.lnu.se>
2019-01-18 23:40:31 +0700

    ProductImage model, update Product, display in admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, ProductImage
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'slug']
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['path']
--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,15 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=100, unique=True, db_index = True)
     description = models.TextField(blank=True)
+    main_image = models.ImageField(upload_to='products_upload/', null=True) 
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+   
+
+class ProductImage(models.Model):
+    path = models.ImageField(upload_to='products_upload/')
+    product = models.ForeignKey(Product,
+                                 related_name='images',
+                                 null=True,
+                                 on_delete=models.CASCADE)
