21c3ae9fc4c76b82d9d363dcd271a6eab30ccd0e
Patrik <patriknilsson1992@gmail.com>
2019-01-18 23:46:22 +0700

    display products in the view, setup static files

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product, ProductImage
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+  
+    return render(request,'shop/products_list.html', {'products': products})
+    
