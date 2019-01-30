838b9f0a3d87391491675420843650b0465ad41f
Patrik <patriknilsson1992@gmail.com>
2019-01-30 17:19:55 +0700

    add order_item and cost method

--- order/models.py
+++ order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from shop.models import Product
 
 class Order(models.Model):
     first_name = models.CharField(max_length=40)
@@ -10,4 +11,25 @@ class Order(models.Model):
     city = models.CharField(max_length=80)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    def __str__(self):
+            return '{}'.format(self.id)
+    def get_cost(self):
+        return self.price * self.quantity
+        
     
