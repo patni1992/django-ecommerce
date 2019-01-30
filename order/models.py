606eca41c88871d44cdca565f68f8e636214ae1b
Patrik <patriknilsson1992@gmail.com>
2019-01-30 17:13:47 +0700

    create order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    paid = models.BooleanField(default=False)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=80)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
