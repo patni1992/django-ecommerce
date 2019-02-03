b37e379a0c2c678e3a5c5ba81d4faa6a9d75161c
Patrik <patriknilsson1992@gmail.com>
2019-02-03 13:07:06 +0700

    update filter product on category to use slug

--- shop/urls.py
+++ shop/urls.py
@@ -7,7 +7,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<slug:category_name>/', views.product_list, 
+    path('<slug:category_slug>/', views.product_list, 
          name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_item,
          name='product_item'),
--- shop/views.py
+++ shop/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 
-def product_list(request, category_name=None):
+def product_list(request, category_slug=None):
     products = Product.objects.all()
     categories = Category.objects.all()
   
-    if category_name:
-        category = get_object_or_404(Category, name=category_name)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
     return render(request,'shop/products_list.html', {
